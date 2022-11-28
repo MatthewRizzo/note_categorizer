60be95402c57894bba53aba5df49e5b9a41ad7de
Matt Rizzo <mattrizzo21@gmail.com>
2022-11-28 12:50:24 -0500

    Defined schema for data input

--- note_categorizer/categorizer/parser.py
+++ note_categorizer/categorizer/parser.py
@@ -4,27 +4,48 @@ it. """
 from typing import Dict
 from typing import List
 from typing import NamedTuple
+from typing import Type, TypeVar
 from dataclasses import dataclass
-
 
 from note_categorizer.common.category import Category
 
+
 class ParsedData(NamedTuple):
     """Represents parsed data"""
+
     known_assignments: Dict[Category, List[str]]
 
     # Notes that still need to be added to a category
     unknown_assignments: List[str]
 
     def is_fully_parsed(self) -> bool:
-        """ # Return
-            * True when there are no more unknown assignments for notes.
-            * False otherwise.
+        """# Return
+        * True when there are no more unknown assignments for notes.
+        * False otherwise.
         """
         return len(self.unknown_assignments) == 0
 
+
+# Added for static type checking on constructor functions
+ParserStatic = TypeVar("ParserStatic", bound="Parser")
+
+
 @dataclass
-class Parser():
+class Parser:
     """Overall parser. Intended to be used as a library for other modules and
     projects."""
+
     valid_categories: List[Category]
+
+    def add_category(self, new_category: Category) -> None:
+        """Adds a category to the list"""
+        self.valid_categories.append(new_category)
+
+    @classmethod
+    def from_json(cls: Type[ParserStatic], serial_list: List[dict]) -> ParserStatic:
+        """Instantiates object of class when the input data is a dictionary.
+        # Precondition
+        The dictionary matches the schema presented by the category class"""
+        category_list: List[Category] = Category.from_serial_list(serial_list)
+
+        return cls(category_list)
--- note_categorizer/common/category.py
+++ note_categorizer/common/category.py
@@ -1,15 +1,73 @@
 """Represents the different categories and how individual notes should
 be grouped together. Maps a category name to its description"""
 
-from dataclasses import dataclass
 from typing import List
+from typing import TypeVar
+from marshmallow import Schema, fields
+from marshmallow.decorators import post_load
+
+# Added for static type checking on constructor functions
+StaticCategory = TypeVar("StaticCategory", bound="Category")
 
 
-@dataclass
-class Category():
+class Category:
     """Represents a category"""
-    # One work name for the category
-    name: str
 
-    # Keywords that ~can~ be asscribed to notes of this category
-    keywords: List[str]
+    def __init__(self, name: str, keywords: List[str]) -> None:
+        """Initializes a category. Makes all keywords lowercase."""
+        # One work name for the category
+        self.name: str = name
+
+        # Keywords that ~can~ be asscribed to notes of this category
+        added_keywords = self.generate_keywords_from_name(keywords)
+        unformatted_keywords = keywords + added_keywords
+        self._keywords: List[str] = list(
+            map(lambda keyword: keyword.lower(), unformatted_keywords)
+        )
+
+    def generate_keywords_from_name(self, default_keywords: List[str]) -> List[str]:
+        """Adds to the list of keywords by generating them from category name.
+        Does not allow for duplicates with existing keywords.
+        # Return
+        * The keywords generated
+        # Parameters
+        * `default_keywords` - The keywords passed to the constructor
+        """
+        name_components = self.name.split(" ")
+        new_keywords = []
+        for component in name_components:
+            if component.lower() not in default_keywords:
+                new_keywords.append(component.lower())
+        return new_keywords
+
+    def is_keyword_present(self, phrase: str) -> bool:
+        """Checks the given phrase to see if any keywords of this category are present.
+        # Return
+            * True if a keyword is present
+            * False otherwise."""
+        lowercase_phrase = phrase.lower()
+        for keyword in self._keywords:
+            if keyword in lowercase_phrase:
+                return True
+        return False
+
+    @classmethod
+    def from_serial_list(cls, serial_list: List[dict]) -> List[StaticCategory]:
+        """Generates a list of instantiated members of the class from a list of
+        serial data representing the class."""
+        return list(
+            map(lambda serial_data: CategorySchema().load(serial_data), serial_list)
+        )
+
+
+class CategorySchema(Schema):
+    """Schema representing a single category. Useful for deserializing data."""
+
+    name = fields.String()
+    keywords = fields.List(fields.String())
+
+    @post_load
+    # pylint: disable=unused-argument
+    def create_category(self, data: dict, **kwargs) -> Category:
+        """Creates a category"""
+        return Category(**data)
